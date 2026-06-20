2d6228a8951db5b987d1e73c32ee4879e0393ebc
Ramo74Dixit <ramodixit76@gmail.com>
2026-06-20 11:21:00 +0530

    hkjdfshlkjhfesd

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from "react";
 function Card(){
     return(
         <div>
-    
+         <h1> This is me</h1>
          <div style={{width:"250px",height:"150px",backgroundColor:"skyblue",border:"1px solid green",borderRadius:"5px"}}>
            <img src="https://m.media-amazon.com/images/I/71EgPH4eJvL._AC_SY95_.jpg" alt=""/>
          </div>
