beef33a369f4fd46b4416bdc5fccefd1b905692f
Ramo74Dixit <ramodixit76@gmail.com>
2026-06-20 11:20:21 +0530

    changes done

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Card(){
+    return(
+        <div>
+    
+         <div style={{width:"250px",height:"150px",backgroundColor:"skyblue",border:"1px solid green",borderRadius:"5px"}}>
+           <img src="https://m.media-amazon.com/images/I/71EgPH4eJvL._AC_SY95_.jpg" alt=""/>
+         </div>
+
+        </div>
+    ) 
+}
+
+export default Card;
